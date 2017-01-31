d8e7fc4916088480a708350dcd5bc75d11c2299b
Michael Ericson <ericsonm@Michaels-MBP.home>
2017-01-31 17:16:02 -0500

    Fix time issue

--- web/static/js/components/DepartureTableRow.js
+++ web/static/js/components/DepartureTableRow.js
@@ -5,7 +5,7 @@ function DepartureTableRow(props) {
   return (
     <tbody>
       <tr className={props.index % 2 === 0 ? props.tableRowClass : null}>
-        <td>{moment(+props.ScheduledTime).format('LT')}</td>
+        <td>{moment(+props.ScheduledTime*1000).format('LT')}</td>
         <td>{props.Destination}</td>
         <td>{props.Trip}</td>
         <td>{props.Track ? props.Track : 'TBD' }</td>
